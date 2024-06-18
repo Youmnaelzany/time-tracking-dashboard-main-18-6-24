b0754d30a2053bd238f6c4a15ca5e759666c39ff
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2024-06-18 18:34:38 +0300

    Setting up the working space Start project with Astro add React & Tailwind  add Shadcn add Prettier  remove node_modules/ package-lock.json install packages with Bun add images folder add CSS resets change project icon add project README file change the title to the project name  add google fonts  start the project with mobile first method
    Setting up the working space
    Start project with Astro
    add React & Tailwind
    add Shadcn
    add Prettier
    remove node_modules/ package-lock.json
    install packages with Bun
    add images folder
    add CSS resets
    change project icon
    add project README file
    change the title to the project name
    add google fonts
    start the project with mobile first method

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  prefix: "",
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
